ce04d957b0d3aa8b2d8b027686511c110bedff96
Vladislav <44534111+Vladik356@users.noreply.github.com>
2024-08-15 02:55:55 +0300

    Update Main.java

--- src/Task_1/Main.java
+++ src/Task_1/Main.java
@@ -7,21 +7,16 @@ public class Main {
                 "Груша", "Яблоко", "Банан", "Персик", "Банан", "Абрикос",
                 "Груша", "Яблоко", "Апельсин"};
 
-        // Создаем HashMap
         Map<String, Integer> wordCount = new HashMap<>();
 
-        // Заполняем HashMap
         for (String word : words) {
             wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
         }
 
-        // Создаем HashSet
         Set<String> uniqueWords = new HashSet<>(wordCount.keySet());
 
-        // Выводим список
         System.out.println("Уникальные слова: " + uniqueWords);
 
-        // Выводим, сколько раз встречается каждое слово
         System.out.println("Количество вхождений каждого слова:");
         for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
